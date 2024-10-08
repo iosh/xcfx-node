a5c4ea1768f666dcfcfbb746798075d14581984d
iosh <ioshx@foxmail.com>
2024-10-08 21:55:39 +0800

    chore: update test

--- __test__/server.test.ts
+++ __test__/server.test.ts
@@ -15,7 +15,40 @@ describe("server", () => {
     });
 
     const status = await client.getStatus();
+
     expect(status.chainId).toBe(1234);
     expect(status.networkId).toBe(1234);
+
+    await server.stop();
+
+    await expect(async () =>
+      client.getStatus(),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(`
+      [TimeoutError: The request took too long to respond.
+
+      URL: http://127.0.0.1:12537
+      Request body: {"method":"cfx_getStatus"}
+
+      Details: The request timed out.
+      Version: 2.21.16]
+    `);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
   });
+
+  // test("custom id 1356", async () => {
+  //   const server = await createServer();
+  //   await server.start();
+  //   // TODO update this
+  //   await new Promise((resolve) => setTimeout(resolve, 15000));
+  //   const client = createPublicClient({
+  //     chain: localChain,
+  //     transport: http(),
+  //   });
+  //   const status = await client.getStatus();
+  //   expect(status.chainId).toBe(1356);
+  //   expect(status.networkId).toBe(1356);
+
+  //   await server.stop();
+  //   await new Promise((resolve) => setTimeout(resolve, 3000));
+  // });
 });
