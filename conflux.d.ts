4c46a34260b105c2bf5703939c5267de271894a2
iosh <ioshx@foxmail.com>
2025-02-01 12:41:58 +0800

    chore: add default value to  poll_lifetime_in_seconds

--- conflux.d.ts
+++ conflux.d.ts
@@ -9,15 +9,30 @@ export declare class ConfluxNode {
 export interface ConfluxConfig {
   /**
    * Set the node type to Full node, Archive node, or Light node.
-   *  Possible values are "full", "archive",or"light".
+   *  Possible values are "full", "archive",or "light".
    * @default "full"
    */
   nodeType?: string
   /**
-   * If it's not set, blocks will only be generated after receiving a transaction.Otherwise,
-   * the blocks are automatically generated every
+   * Database type to store block-related data.
+   * Supported: rocksdb, sqlite.
+   * @default sqlite
    */
-  devBlockIntervalMs?: number
+  blockDbType?: string
+  /** @default: temp dir */
+  confluxDataDir?: string
+  /**
+   * The chain ID of the network.(core space)
+   * @default 1234
+   */
+  chainId?: number
+  /**
+   * The chain ID of the network.( eSpace)
+   *  @default 1235
+   */
+  evmChainId?: number
+  /** bootnodes is a list of nodes that a conflux node trusts, and will be used to sync the blockchain when a node starts. */
+  bootnodes?: string
   /** mining_author is the address to receive mining rewards. */
   miningAuthor?: string
   /**
@@ -32,24 +47,25 @@ export interface ConfluxConfig {
   miningType?: string
   /** Port for stratum. */
   stratumPort?: number
-  /** log_conf` the path of the log4rs configuration file. The configuration in the file will overwrite the value set by `log_level`. */
-  logConf?: string
+  /** Secret key for stratum. The value is 64-digit hex string. If not set, the RPC subscription will not check the authorization. */
+  stratumSecret?: string
+  /** Window size for PoW manager */
+  powProblemWindowSize?: number
   /**
-   * `log_file` is the path of the log file"
-   * If not set, the log will only be printed to stdout, and not persisted to files.
+   * If it's not set, blocks will only be generated after receiving a transaction.Otherwise,
+   * the blocks are automatically generated every
    */
-  logFile?: string
+  devBlockIntervalMs?: number
   /**
-   * log_level` is the printed log level.
-   * "error" | "warn" | "info" | "debug" | "trace" | "off"
+   * If it's `true`, `DEFERRED_STATE_EPOCH_COUNT` blocks are generated after
+   * receiving a new tx through RPC calling to pack and execute this
+   * transaction
    */
-  logLevel?: string
-  /**
-   * The port of the websocket JSON-RPC server(public_rpc_apis is user defined).
-   * if not set, the JSON-RPC server will not be started.
-   * @default null
-   */
-  jsonrpcWsPort?: number
+  devPackTxImmediately?: boolean
+  /**  The private key of the genesis (core space), every account will be receive 10000 CFX */
+  genesisSecrets?: Array<string>
+  /**  The private key of the genesis (eSpace), every account will be receive 10000 CFX */
+  genesisEvmSecrets?: Array<string>
   /**
    * `tcp_port` is the TCP port that the process listens for P2P messages. The default is 32323.
    * @default 32323
@@ -60,6 +76,8 @@ export interface ConfluxConfig {
    * @default 32323
    */
   udpPort?: number
+  /** `public_address` is the address of this node used */
+  publicAddress?: string
   /**
    * Possible Core space names are: all, safe, cfx, pos, debug, pubsub, test, trace, txpool.
    * `safe` only includes `cfx` and `pubsub`, `txpool`.
@@ -72,75 +90,11 @@ export interface ConfluxConfig {
    */
   publicEvmRpcApis?: string
   /**
-   * The chain ID of the network.(core space)
-   * @default 1234
+   * The port of the websocket JSON-RPC server(public_rpc_apis is user defined).
+   * if not set, the JSON-RPC server will not be started.
+   * @default null
    */
-  chainId?: number
-  /**
-   * The chain ID of the network.( eSpace)
-   *  @default 1235
-   */
-  evmChainId?: number
-  /**
-   * The password used to encrypt the private key of the pos_private_key.
-   *  @default "123456"
-   */
-  devPosPrivateKeyEncryptionPassword?: string
-  /**  The private key of the genesis (core space), every account will be receive 10000 CFX */
-  genesisSecrets?: Array<string>
-  /**  The private key of the genesis (eSpace), every account will be receive 10000 CFX */
-  genesisEvmSecrets?: Array<string>
-  /**
-   * If it's `true`, `DEFERRED_STATE_EPOCH_COUNT` blocks are generated after
-   * receiving a new tx through RPC calling to pack and execute this
-   * transaction
-   */
-  devPackTxImmediately?: boolean
-  /** @default:1 */
-  posReferenceEnableHeight?: number
-  /** @default:2 */
-  defaultTransitionTime?: number
-  /** @default:3 */
-  cip1559TransitionHeight?: number
-  /**
-   * Enable CIP43A, CIP64, CIP71, CIP78A, CIP92 after hydra_transition_number
-   * @default:1
-   */
-  hydraTransitionNumber?: number
-  /**
-   * Enable cip76, cip86 after hydra_transition_height
-   * @default:1
-   */
-  hydraTransitionHeight?: number
-  /** @default: temp dir */
-  confluxDataDir?: string
-  /** pos config path */
-  posConfigPath?: string
-  /** pos initial_nodes.json path */
-  posInitialNodesPath?: string
-  /** pos pos_key file path */
-  posPrivateKeyPath?: string
-  /**
-   * Database type to store block-related data.
-   * Supported: rocksdb, sqlite.
-   * @default sqlite
-   */
-  blockDbType?: string
-  /** bootnodes is a list of nodes that a conflux node trusts, and will be used to sync the blockchain when a node starts. */
-  bootnodes?: string
-  /** Window size for PoW manager */
-  powProblemWindowSize?: number
-  /** # Secret key for stratum. The value is 64-digit hex string. If not set, the RPC subscription will not check the authorization. */
-  stratumSecret?: string
-  /** `public_address` is the address of this node used */
-  publicAddress?: string
-  /**
-   * `jsonrpc_http_keep_alive` is used to control whether to set KeepAlive for rpc HTTP connections.
-   * @default false
-   */
-  jsonrpcHttpKeepAlive?: boolean
-  /** `print_memory_usage_period_s` is the period for printing memory usage. */
-  printMemoryUsagePeriodS?: number
+  jsonrpcWsPort?: number
   /**
    * The port of the http JSON-RPC server.public_rpc_apis is user defined).
    * if not set, the JSON-RPC server will not be started.
@@ -184,10 +138,57 @@ export interface ConfluxConfig {
    */
   jsonrpcLocalWsPort?: number
   /**
+   * `jsonrpc_http_keep_alive` is used to control whether to set KeepAlive for rpc HTTP connections.
+   * @default false
+   */
+  jsonrpcHttpKeepAlive?: boolean
+  /**
+   * The password used to encrypt the private key of the pos_private_key.
+   *  @default "123456"
+   */
+  devPosPrivateKeyEncryptionPassword?: string
+  /** @default:1 */
+  posReferenceEnableHeight?: number
+  /** pos config path */
+  posConfigPath?: string
+  /** pos initial_nodes.json path */
+  posInitialNodesPath?: string
+  /** pos pos_key file path */
+  posPrivateKeyPath?: string
+  /** @default:2 */
+  defaultTransitionTime?: number
+  /** @default:3 */
+  cip1559TransitionHeight?: number
+  /**
+   * Enable CIP43A, CIP64, CIP71, CIP78A, CIP92 after hydra_transition_number
+   * @default:1
+   */
+  hydraTransitionNumber?: number
+  /**
+   * Enable cip76, cip86 after hydra_transition_height
+   * @default:1
+   */
+  hydraTransitionHeight?: number
+  /** log_conf` the path of the log4rs configuration file. The configuration in the file will overwrite the value set by `log_level`. */
+  logConf?: string
+  /**
+   * `log_file` is the path of the log file"
+   * If not set, the log will only be printed to stdout, and not persisted to files.
+   */
+  logFile?: string
+  /**
+   * log_level` is the printed log level.
+   * "error" | "warn" | "info" | "debug" | "trace" | "off"
+   */
+  logLevel?: string
+  /** `print_memory_usage_period_s` is the period for printing memory usage. */
+  printMemoryUsagePeriodS?: number
+  /**
    * `poll_lifetime_in_seconds` is the lifetime of the poll in seconds.
    * If set, the following RPC methods will be enabled:
    * - `cfx_newFilter` `cfx_newBlockFilter` `cfx_newPendingTransactionFilter` `cfx_getFilterChanges` `cfx_getFilterLogs` `cfx_uninstallFilter`.
    * - `eth_newFilter` `eth_newBlockFilter` `eth_newPendingTransactionFilter` eth_getFilterChanges eth_getFilterLogs eth_uninstallFilter
+   * @default: 60
    */
   pollLifetimeInSeconds?: number
   /** if `get_logs_filter_max_limit` is configured but the query would return more logs */
--- src/config.rs
+++ src/config.rs
@@ -11,109 +11,96 @@ use std::{
 #[napi(object)]
 #[derive(Debug)]
 pub struct ConfluxConfig {
+  // ============= Node Configuration =============
   /// Set the node type to Full node, Archive node, or Light node.
-  ///  Possible values are "full", "archive",or"light".
+  ///  Possible values are "full", "archive",or "light".
   /// @default "full"
   pub node_type: Option<String>,
-  //   /// `dev` mode is for users to run a single node that automatically
-  //   ///  @default "dev"
-  //   pub mode: Option<String>,
-  /// If it's not set, blocks will only be generated after receiving a transaction.Otherwise,
-  /// the blocks are automatically generated every
-  pub dev_block_interval_ms: Option<i64>,
-  /// mining_author is the address to receive mining rewards.
-  pub mining_author: Option<String>,
-  /// Listen address for stratum
-  /// @default "127.0.0.1"
-  pub stratum_listen_address: Option<String>,
-  /// `mining_type` is the type of mining.
-  /// stratum | cpu | disable
-  pub mining_type: Option<String>,
-  /// Port for stratum.
-  pub stratum_port: Option<u16>,
-  /// log_conf` the path of the log4rs configuration file. The configuration in the file will overwrite the value set by `log_level`.
-  pub log_conf: Option<String>,
-  /// `log_file` is the path of the log file"
-  /// If not set, the log will only be printed to stdout, and not persisted to files.
-  pub log_file: Option<String>,
-  /// log_level` is the printed log level.
-  /// "error" | "warn" | "info" | "debug" | "trace" | "off"
-  pub log_level: Option<String>,
-  /// The port of the websocket JSON-RPC server(public_rpc_apis is user defined).
-  /// if not set, the JSON-RPC server will not be started.
-  /// @default null
-  pub jsonrpc_ws_port: Option<u16>,
 
-  /// `tcp_port` is the TCP port that the process listens for P2P messages. The default is 32323.
-  /// @default 32323
-  pub tcp_port: Option<u16>,
-  /// `udp_port` is the UDP port used for node discovery.
-  /// @default 32323
-  pub udp_port: Option<u16>,
-
-  /// Possible Core space names are: all, safe, cfx, pos, debug, pubsub, test, trace, txpool.
-  /// `safe` only includes `cfx` and `pubsub`, `txpool`.
-  ///  @default "all"
-  pub public_rpc_apis: Option<String>,
-  /// Possible eSpace names are: eth, ethpubsub, ethdebug.
-  ///  @default 'evm'
-  pub public_evm_rpc_apis: Option<String>,
-  /// The chain ID of the network.(core space)
-  /// @default 1234
-  pub chain_id: Option<u32>,
-  /// The chain ID of the network.( eSpace)
-  ///  @default 1235
-  pub evm_chain_id: Option<u32>,
-  /// The password used to encrypt the private key of the pos_private_key.
-  ///  @default "123456"
-  pub dev_pos_private_key_encryption_password: Option<String>,
-  ///  The private key of the genesis (core space), every account will be receive 10000 CFX
-  pub genesis_secrets: Option<Vec<String>>,
-  ///  The private key of the genesis (eSpace), every account will be receive 10000 CFX
-  pub genesis_evm_secrets: Option<Vec<String>>,
-  /// If it's `true`, `DEFERRED_STATE_EPOCH_COUNT` blocks are generated after
-  /// receiving a new tx through RPC calling to pack and execute this
-  /// transaction
-  pub dev_pack_tx_immediately: Option<bool>,
-  /// @default:1
-  pub pos_reference_enable_height: Option<i64>,
-  /// @default:2
-  pub default_transition_time: Option<i64>,
-  /// @default:3
-  pub cip1559_transition_height: Option<i64>,
-  /// Enable CIP43A, CIP64, CIP71, CIP78A, CIP92 after hydra_transition_number
-  /// @default:1
-  pub hydra_transition_number: Option<i64>,
-  /// Enable cip76, cip86 after hydra_transition_height
-  /// @default:1
-  pub hydra_transition_height: Option<i64>,
-  /// @default: temp dir
-  pub conflux_data_dir: Option<String>,
-  /// pos config path
-  pub pos_config_path: Option<String>,
-  /// pos initial_nodes.json path
-  pub pos_initial_nodes_path: Option<String>,
-  /// pos pos_key file path
-  pub pos_private_key_path: Option<String>,
   /// Database type to store block-related data.
   /// Supported: rocksdb, sqlite.
   /// @default sqlite
   pub block_db_type: Option<String>,
 
+  /// @default: temp dir
+  pub conflux_data_dir: Option<String>,
+
+  // ============= Chain Configuration =============
+  /// The chain ID of the network.(core space)
+  /// @default 1234
+  pub chain_id: Option<u32>,
+
+  /// The chain ID of the network.( eSpace)
+  ///  @default 1235
+  pub evm_chain_id: Option<u32>,
+
   /// bootnodes is a list of nodes that a conflux node trusts, and will be used to sync the blockchain when a node starts.
   pub bootnodes: Option<String>,
 
+  // ============= Mining Configuration =============
+  /// mining_author is the address to receive mining rewards.
+  pub mining_author: Option<String>,
+
+  /// Listen address for stratum
+  /// @default "127.0.0.1"
+  pub stratum_listen_address: Option<String>,
+
+  /// `mining_type` is the type of mining.
+  /// stratum | cpu | disable
+  pub mining_type: Option<String>,
+
+  /// Port for stratum.
+  pub stratum_port: Option<u16>,
+
+  /// Secret key for stratum. The value is 64-digit hex string. If not set, the RPC subscription will not check the authorization.
+  pub stratum_secret: Option<String>,
+
   /// Window size for PoW manager
   pub pow_problem_window_size: Option<u32>,
-  /// # Secret key for stratum. The value is 64-digit hex string. If not set, the RPC subscription will not check the authorization.
-  pub stratum_secret: Option<String>,
+
+  // ============= Development Mode Configuration =============
+  /// If it's not set, blocks will only be generated after receiving a transaction.Otherwise,
+  /// the blocks are automatically generated every
+  pub dev_block_interval_ms: Option<i64>,
+
+  /// If it's `true`, `DEFERRED_STATE_EPOCH_COUNT` blocks are generated after
+  /// receiving a new tx through RPC calling to pack and execute this
+  /// transaction
+  pub dev_pack_tx_immediately: Option<bool>,
+
+  ///  The private key of the genesis (core space), every account will be receive 10000 CFX
+  pub genesis_secrets: Option<Vec<String>>,
+
+  ///  The private key of the genesis (eSpace), every account will be receive 10000 CFX
+  pub genesis_evm_secrets: Option<Vec<String>>,
+
+  // ============= Network Configuration =============
+  /// `tcp_port` is the TCP port that the process listens for P2P messages. The default is 32323.
+  /// @default 32323
+  pub tcp_port: Option<u16>,
+
+  /// `udp_port` is the UDP port used for node discovery.
+  /// @default 32323
+  pub udp_port: Option<u16>,
+
   /// `public_address` is the address of this node used
   pub public_address: Option<String>,
-  /// `jsonrpc_http_keep_alive` is used to control whether to set KeepAlive for rpc HTTP connections.
-  /// @default false
-  pub jsonrpc_http_keep_alive: Option<bool>,
-  /// `print_memory_usage_period_s` is the period for printing memory usage.
-  pub print_memory_usage_period_s: Option<u32>,
+
+  // ============= JSON-RPC Configuration =============
+  /// Possible Core space names are: all, safe, cfx, pos, debug, pubsub, test, trace, txpool.
+  /// `safe` only includes `cfx` and `pubsub`, `txpool`.
+  ///  @default "all"
+  pub public_rpc_apis: Option<String>,
+
+  /// Possible eSpace names are: eth, ethpubsub, ethdebug.
+  ///  @default 'evm'
+  pub public_evm_rpc_apis: Option<String>,
+
+  /// The port of the websocket JSON-RPC server(public_rpc_apis is user defined).
+  /// if not set, the JSON-RPC server will not be started.
+  /// @default null
+  pub jsonrpc_ws_port: Option<u16>,
+
   /// The port of the http JSON-RPC server.public_rpc_apis is user defined).
   /// if not set, the JSON-RPC server will not be started.
   /// @default null
@@ -128,27 +115,86 @@ pub struct ConfluxConfig {
   /// if not set, the JSON-RPC server will not be started.
   /// @default null
   pub jsonrpc_http_eth_port: Option<u16>,
+
   /// The port of the websocket JSON-RPC serverpublic_rpc_apis is user defined).
   /// if not set, the JSON-RPC server will not be started.
   /// @default null
   pub jsonrpc_ws_eth_port: Option<u16>,
+
   /// The port of the tcp JSON-RPC server(public_rpc_apis is "all").
   /// if not set, the JSON-RPC server will not be started.
   /// @default null
   pub jsonrpc_local_tcp_port: Option<u16>,
+
   /// The port of the http JSON-RPC server(public_rpc_apis is "all").
   /// if not set, the JSON-RPC server will not be started.
   /// @default null
   pub jsonrpc_local_http_port: Option<u16>,
+
   /// The port of the websocket JSON-RPC server(public_rpc_apis is "all").
   /// if not set, the JSON-RPC server will not be started.
   /// @default null
   pub jsonrpc_local_ws_port: Option<u16>,
+
+  /// `jsonrpc_http_keep_alive` is used to control whether to set KeepAlive for rpc HTTP connections.
+  /// @default false
+  pub jsonrpc_http_keep_alive: Option<bool>,
+
+  // ============= PoS Configuration =============
+  /// The password used to encrypt the private key of the pos_private_key.
+  ///  @default "123456"
+  pub dev_pos_private_key_encryption_password: Option<String>,
+
+  /// @default:1
+  pub pos_reference_enable_height: Option<i64>,
+
+  /// pos config path
+  pub pos_config_path: Option<String>,
+
+  /// pos initial_nodes.json path
+  pub pos_initial_nodes_path: Option<String>,
+
+  /// pos pos_key file path
+  pub pos_private_key_path: Option<String>,
+
+  // ============= Protocol Upgrade Configuration =============
+  /// @default:2
+  pub default_transition_time: Option<i64>,
+
+  /// @default:3
+  pub cip1559_transition_height: Option<i64>,
+
+  /// Enable CIP43A, CIP64, CIP71, CIP78A, CIP92 after hydra_transition_number
+  /// @default:1
+  pub hydra_transition_number: Option<i64>,
+
+  /// Enable cip76, cip86 after hydra_transition_height
+  /// @default:1
+  pub hydra_transition_height: Option<i64>,
+
+  // ============= Logging Configuration =============
+  /// log_conf` the path of the log4rs configuration file. The configuration in the file will overwrite the value set by `log_level`.
+  pub log_conf: Option<String>,
+
+  /// `log_file` is the path of the log file"
+  /// If not set, the log will only be printed to stdout, and not persisted to files.
+  pub log_file: Option<String>,
+
+  /// log_level` is the printed log level.
+  /// "error" | "warn" | "info" | "debug" | "trace" | "off"
+  pub log_level: Option<String>,
+
+  /// `print_memory_usage_period_s` is the period for printing memory usage.
+  pub print_memory_usage_period_s: Option<u32>,
+
+  // ============= Filter and Poll Configuration =============
   /// `poll_lifetime_in_seconds` is the lifetime of the poll in seconds.
   /// If set, the following RPC methods will be enabled:
   /// - `cfx_newFilter` `cfx_newBlockFilter` `cfx_newPendingTransactionFilter` `cfx_getFilterChanges` `cfx_getFilterLogs` `cfx_uninstallFilter`.
   /// - `eth_newFilter` `eth_newBlockFilter` `eth_newPendingTransactionFilter` eth_getFilterChanges eth_getFilterLogs eth_uninstallFilter
+  /// @default: 60
   pub poll_lifetime_in_seconds: Option<u32>,
+
   /// if `get_logs_filter_max_limit` is configured but the query would return more logs
   pub get_logs_filter_max_limit: Option<u32>,
 }
@@ -156,18 +202,42 @@ pub struct ConfluxConfig {
 pub fn convert_config(js_config: ConfluxConfig, temp_dir_path: &Path) -> Configuration {
   let mut conf = Configuration::default();
 
+  // ============= Node Configuration =============
   let node_type = match js_config.node_type.as_deref() {
     Some("archive") => NodeType::Archive,
     Some("light") => NodeType::Light,
     _ => NodeType::Full,
   };
-  // node_type
   conf.raw_conf.node_type = Some(node_type);
 
-  // chain
-  conf.raw_conf.chain_id = Some(js_config.chain_id.unwrap_or(1234));
+  conf.raw_conf.block_db_type = js_config.block_db_type.unwrap_or("sqlite".to_string());
 
+  conf.raw_conf.conflux_data_dir = js_config
+    .conflux_data_dir
+    .unwrap_or(String::from(temp_dir_path.to_str().unwrap()));
+
+  // ============= Chain Configuration =============
+  conf.raw_conf.chain_id = Some(js_config.chain_id.unwrap_or(1234));
   conf.raw_conf.evm_chain_id = Some(js_config.evm_chain_id.unwrap_or(1235));
+  conf.raw_conf.bootnodes = js_config.bootnodes;
+
+  // ============= Mining Configuration =============
+  conf.raw_conf.mining_author = js_config.mining_author;
+  conf.raw_conf.stratum_listen_address = js_config
+    .stratum_listen_address
+    .unwrap_or("127.0.0.1".to_string());
+  conf.raw_conf.mining_type = js_config.mining_type;
+  conf.raw_conf.stratum_port = js_config.stratum_port.unwrap_or(32525);
+  conf.raw_conf.stratum_secret = js_config.stratum_secret;
+  conf.raw_conf.pow_problem_window_size = js_config.pow_problem_window_size.unwrap_or(1) as usize;
+
+  // ============= Development Mode Configuration =============
+  conf.raw_conf.mode = Some("dev".to_string());
+  conf.raw_conf.dev_block_interval_ms = match js_config.dev_block_interval_ms {
+    Some(n) => Some(n as u64),
+    _ => None,
+  };
+  conf.raw_conf.dev_pack_tx_immediately = js_config.dev_pack_tx_immediately;
 
   if let Some(pk) = js_config.genesis_secrets {
     conf.raw_conf.genesis_secrets = write_secrets_to_file(pk, "genesis_secrets.txt", temp_dir_path);
@@ -177,16 +247,11 @@ pub fn convert_config(js_config: ConfluxConfig, temp_dir_path: &Path) -> Configu
       write_secrets_to_file(pk, "genesis_evm_secrets.txt", temp_dir_path);
   }
 
-  // mode
-  conf.raw_conf.mode = Some("dev".to_string());
+  // ============= Network Configuration =============
+  conf.raw_conf.tcp_port = js_config.tcp_port.unwrap_or(32323);
+  conf.raw_conf.udp_port = js_config.udp_port.or(Some(32323));
 
-  conf.raw_conf.dev_block_interval_ms = match js_config.dev_block_interval_ms {
-    Some(n) => Some(n as u64),
-    _ => None,
-  };
-
-  conf.raw_conf.mining_author = js_config.mining_author;
-
+  // ============= JSON-RPC Configuration =============
   conf.raw_conf.public_rpc_apis = match js_config.public_rpc_apis {
     Some(s) => ApiSet::from_str(&s).unwrap_or(ApiSet::All),
     _ => ApiSet::All,
@@ -197,86 +262,52 @@ pub fn convert_config(js_config: ConfluxConfig, temp_dir_path: &Path) -> Configu
     _ => ApiSet::Evm,
   };
 
+  conf.raw_conf.jsonrpc_ws_port = js_config.jsonrpc_ws_port;
+  conf.raw_conf.jsonrpc_http_port = js_config.jsonrpc_http_port;
+  conf.raw_conf.jsonrpc_tcp_port = js_config.jsonrpc_tcp_port;
+  conf.raw_conf.jsonrpc_http_eth_port = js_config.jsonrpc_http_eth_port;
+  conf.raw_conf.jsonrpc_ws_eth_port = js_config.jsonrpc_ws_eth_port;
+  conf.raw_conf.jsonrpc_local_tcp_port = js_config.jsonrpc_local_tcp_port;
+  conf.raw_conf.jsonrpc_local_http_port = js_config.jsonrpc_local_http_port;
+  conf.raw_conf.jsonrpc_local_ws_port = js_config.jsonrpc_local_ws_port;
+  conf.raw_conf.jsonrpc_http_keep_alive = js_config.jsonrpc_http_keep_alive.unwrap_or(false);
+
+  // ============= PoS Configuration =============
   conf.raw_conf.dev_pos_private_key_encryption_password = Some(
     js_config
       .dev_pos_private_key_encryption_password
       .unwrap_or("123456".to_string()),
   );
-
-  conf.raw_conf.dev_pack_tx_immediately = js_config.dev_pack_tx_immediately;
-
   conf.raw_conf.pos_reference_enable_height =
     js_config.pos_reference_enable_height.unwrap_or(0) as u64;
-
-  conf.raw_conf.default_transition_time =
-    Some(js_config.default_transition_time.unwrap_or(1) as u64);
-
-  conf.raw_conf.cip1559_transition_height =
-    Some(js_config.cip1559_transition_height.unwrap_or(2) as u64);
-  conf.raw_conf.hydra_transition_number =
-    Some(js_config.hydra_transition_number.unwrap_or(1) as u64);
-  conf.raw_conf.hydra_transition_height = Some(js_config.hydra_transition_height.unwrap_or(1) as u64);
-
-  conf.raw_conf.poll_lifetime_in_seconds = js_config.poll_lifetime_in_seconds;
-
-  conf.raw_conf.get_logs_filter_max_limit = js_config.get_logs_filter_max_limit.map(|n| n as usize);
-
-  // confix data dir, default to temp dir
-  conf.raw_conf.conflux_data_dir = js_config
-    .conflux_data_dir
-    .unwrap_or(String::from(temp_dir_path.to_str().unwrap()));
-  // pos configs
   conf.raw_conf.pos_config_path = js_config
     .pos_config_path
     .or(Some("./pos_config/pos_config.yaml".to_string()));
-
   conf.raw_conf.pos_initial_nodes_path = js_config
     .pos_initial_nodes_path
     .unwrap_or("./pos_config/initial_nodes.json".to_string());
-
   conf.raw_conf.pos_private_key_path = js_config
     .pos_private_key_path
     .unwrap_or("./pos_config/pos_key".to_string());
 
-  // set the block db to sqlite
-  conf.raw_conf.block_db_type = js_config.block_db_type.unwrap_or("sqlite".to_string());
+  // ============= Protocol Upgrade Configuration =============
+  conf.raw_conf.default_transition_time =
+    Some(js_config.default_transition_time.unwrap_or(1) as u64);
+  conf.raw_conf.cip1559_transition_height =
+    Some(js_config.cip1559_transition_height.unwrap_or(2) as u64);
+  conf.raw_conf.hydra_transition_number =
+    Some(js_config.hydra_transition_number.unwrap_or(1) as u64);
+  conf.raw_conf.hydra_transition_height =
+    Some(js_config.hydra_transition_height.unwrap_or(1) as u64);
+
+  // ============= Logging Configuration =============
   conf.raw_conf.log_conf = js_config.log_conf;
-
-  // network
-
-  conf.raw_conf.stratum_listen_address = js_config
-    .stratum_listen_address
-    .unwrap_or("127.0.0.1".to_string());
-
-  conf.raw_conf.jsonrpc_ws_port = js_config.jsonrpc_ws_port;
-  conf.raw_conf.mining_type = js_config.mining_type;
-  conf.raw_conf.jsonrpc_http_port = js_config.jsonrpc_http_port;
-
-  conf.raw_conf.stratum_port = js_config.stratum_port.unwrap_or(32525);
-
-  conf.raw_conf.tcp_port = js_config.tcp_port.unwrap_or(32323);
-
-  conf.raw_conf.udp_port = js_config.udp_port.or(Some(32323));
-
-  // unnecessary config
-  conf.raw_conf.bootnodes = js_config.bootnodes;
-
-  conf.raw_conf.pow_problem_window_size = js_config.pow_problem_window_size.unwrap_or(1) as usize;
-
-  conf.raw_conf.stratum_secret = js_config.stratum_secret;
-
-  conf.raw_conf.jsonrpc_http_keep_alive = js_config.jsonrpc_http_keep_alive.unwrap_or(false);
-
-  conf.raw_conf.jsonrpc_tcp_port = js_config.jsonrpc_tcp_port;
-  conf.raw_conf.jsonrpc_http_eth_port = js_config.jsonrpc_http_eth_port;
-  conf.raw_conf.jsonrpc_ws_eth_port = js_config.jsonrpc_ws_eth_port;
-
-  conf.raw_conf.jsonrpc_local_tcp_port = js_config.jsonrpc_local_tcp_port;
-  conf.raw_conf.jsonrpc_local_http_port = js_config.jsonrpc_local_http_port;
-  conf.raw_conf.jsonrpc_local_ws_port = js_config.jsonrpc_local_ws_port;
-
   conf.raw_conf.print_memory_usage_period_s =
     js_config.print_memory_usage_period_s.map(|x| x as u64);
+
+  // ============= Filter and Poll Configuration =============
+  conf.raw_conf.poll_lifetime_in_seconds = Some(js_config.poll_lifetime_in_seconds.unwrap_or(60));
+  conf.raw_conf.get_logs_filter_max_limit = js_config.get_logs_filter_max_limit.map(|n| n as usize);
 
   conf
 }
